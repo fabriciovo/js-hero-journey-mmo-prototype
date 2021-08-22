de335c968dea09b8d9f81b7d14bd884b991f4b2e
Fabricio <fabricio.v.o@hotmail.com>
2021-08-22 17:14:13 -0300

    Add map class

--- assets/js/Scenes/GameScene.js
+++ assets/js/Scenes/GameScene.js
@@ -27,7 +27,7 @@ class GameScene extends Phaser.Scene {
     }
 
     createPlayer() {
-        this.player = new Player(this, 128, 128, 'characters', 0);
+        this.player = new Player(this, 200, 200, 'characters', 0);
     }
     createChest() {
         this.chests = this.physics.add.group();
@@ -45,7 +45,7 @@ class GameScene extends Phaser.Scene {
         this.wall.setImmovable();
     }
     addCollisions() {
-        this.physics.add.collider(this.player, this.blocked);
+        this.physics.add.collider(this.player, this.map.blocked);
         this.physics.add.overlap(this.player, this.chests, this.collectChest, null, this);
     }
 
@@ -56,21 +56,6 @@ class GameScene extends Phaser.Scene {
     }
 
     createMap(){
-        this.map = this.make.tilemap({key:'map'});
-        console.log(this.map)
-        this.tiles = this.map.addTilesetImage('colored','colored_packed',16,16);
-
-        this.background =  this.map.createLayer(0, this.tiles, 0, 0);
-        
-        this.blocked =  this.map.createLayer(1, this.tiles, 0, 0);
-        this.blocked.setCollisionByExclusion([-1]);
-
-        this.background.setScale(2);
-        this.blocked.setScale(2);
-
-        this.physics.world.bounds.width = this.map.widthInPixels * 2;
-        this.physics.world.bounds.height = this.map.heightInPixels * 2;
-
-        this.cameras.main.setBounds(0,0,this.map.widthInPixels * 2, this.map.heightInPixels * 2)
+        this.map = new Map(this, 'map','colored');
     }
 }
