7590118ce9faf0bf5be2e52fae9c69691c6ffbd1
Fabricio Varisco Oliveira <fabricio.hitsubato@carteiraglobal.com>
2021-09-20 00:14:33 -0300

    deploy

--- build/game_manager/GameManager.js
+++ build/game_manager/GameManager.js
@@ -266,6 +266,7 @@ var GameManager = /*#__PURE__*/function () {
             _this2.players[socket.id].y = playerData.y;
             _this2.players[socket.id].flipX = playerData.flipX;
             _this2.players[socket.id].actionAActive = playerData.actionAActive;
+            _this2.players[socket.id].actionBActive = playerData.actionBActive;
             _this2.players[socket.id].currentDirection = playerData.currentDirection; // emit a message to all players about the player that moved
 
             _this2.io.emit("playerMoved", _this2.players[socket.id]);
