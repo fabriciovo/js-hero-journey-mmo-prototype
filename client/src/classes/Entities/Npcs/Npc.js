43d9af102909bd93364a409af04c8606cb4ae69b
Fabricio Varisco Oliveira <fabricio.hitsubato@carteiraglobal.com>
2021-10-02 22:44:39 -0300

    Update Npc.js

--- client/src/classes/Entities/Npcs/Npc.js
+++ client/src/classes/Entities/Npcs/Npc.js
@@ -1,45 +1,16 @@
 import * as Phaser from "phaser";
 import Entity from "../Entity";
-export default class Npc extends Phaser.Physics.Arcade.Sprite {
+export default class Npc extends Entity {
   constructor(scene, x, y, key, frame, id) {
-    super(scene, x, y, key, frame);
-    this.scene = scene;
-    this.id = id;
-    
-    // enable physics
-    this.scene.physics.world.enable(this);
-    // set immovable if another object collides with our monster
-    this.setImmovable(false);
-    // scale our monster
-    this.setScale(2);
-    // collide with world bounds
-    this.setCollideWorldBounds(true);
-    // add the monster to our existing scene
-    this.scene.add.existing(this);
-    // update the origin
-    this.setOrigin(0);
-  }
-
-  makeActive() {
-    this.setActive(true);
-    this.setVisible(true);
-    this.body.checkCollision.none = false;
-    this.updateHealthBar();
-  }
-
-  makeInactive() {
-    this.setActive(false);
-    this.setVisible(false);
-    this.body.checkCollision.none = true;
-    this.healthBar.clear();
+    super(scene, x, y, key, frame, id);
   }
 
   action(scene,player){
     console.log("sadopkadsk")
     if(player){
-      scene.toggleShop(player, true);
+      scene.shopWindow.showWindow(player);
     }else{
-      scene.toggleShop(player, false);
+      scene.shopWindow.hideWindow();
     }
   }
 }
