2d271adc4a389147cf86fa4704ffc6a24c7901d7
Fabricio Varisco <fabricio.hitsubato@carteiraglobal.com>
2021-12-06 19:56:35 -0300

    aa

--- client/src/classes/UI/Bar.js
+++ client/src/classes/UI/Bar.js
@@ -10,37 +10,37 @@ export default class Bar extends Phaser.Physics.Arcade.Image {
     this.size = size;
     
     this.scene.add.existing(this);
-    this.CreateBar(x, y, depth);
+    //this.CreateBar(x, y, depth);
   }
 
   CreateBar(x, y, depth) {
-    if (this.holder) {
-      this.holderBar = this.scene.add
-        .image(x, y, this.key, this.holder)
-        .setScale(3.5)
-        .setOrigin(0, 0.5)
-        .setDepth(depth);
-    }
-    this.bar = this.scene.add
-      .image(x, y, this.key, this.frame)
-      .setScale(3.5)
-      .setOrigin(0, 0.5)
-      .setDepth(depth);
-    this.bar.displayWidth = this.size;
+    // if (this.holder) {
+    //   this.holderBar = this.scene.add
+    //     .image(x, y, this.key, this.holder)
+    //     .setScale(3.5)
+    //     .setOrigin(0, 0.5)
+    //     .setDepth(depth);
+    // }
+    // this.bar = this.scene.add
+    //   .image(x, y, this.key, this.frame)
+    //   .setScale(3.5)
+    //   .setOrigin(0, 0.5)
+    //   .setDepth(depth);
+    // this.bar.displayWidth = this.size;
   }
 
   UpdateBar(x, y, value, maxValue) {
-    this.bar.setPosition(x, y);
-    this.holderBar.setPosition(x, y);
-    this.bar.displayWidth = (value / maxValue) * this.size;
-    this.holderBar.displayWidth = this.size;
+    // this.bar.setPosition(x, y);
+    // this.holderBar.setPosition(x, y);
+    // this.bar.displayWidth = (value / maxValue) * this.size;
+    // this.holderBar.displayWidth = this.size;
 
   }
 
   DestroyBar() {
-    this.bar.setVisible(false);
-    this.holderBar.setVisible(false);
-    this.bar.destroy();
-    this.holderBar.destroy();
+    // this.bar.setVisible(false);
+    // this.holderBar.setVisible(false);
+    // this.bar.destroy();
+    // this.holderBar.destroy();
   }
 }
