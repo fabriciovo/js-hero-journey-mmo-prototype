58967fd8c088fec613024e454f1330ec9ac4736a
Fabricio Oliveira <13788356+fabriciovo@users.noreply.github.com>
2024-06-23 02:00:50 -0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -14,12 +14,12 @@ import GameManager from "./game_manager/GameManager";
 
 const app = express();
 const server = require("https").Server(app);
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: process.env.CORS_ORIGIN,
-//   },
-// });
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST"]
+  }
+});
 const gameManager = new GameManager(io);
 gameManager.setup();
 const port = process.env.PORT || 3000;
